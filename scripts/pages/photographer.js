962f1c6b71a8d9538be59a6016f9f2100203291a
baya laadjel <bata_cher@hotmail.fr>
2023-04-15 06:22:31 +0200

    recup data with id

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,25 @@
 //Mettre le code JavaScript lié à la page photographer.html
+async function getOnePhotographer(id) {
+
+  const req = await fetch('./data/photographers.json')
+  const resp = await req.json()
+
+  // Faire une recherche javascript sur resp avec l'id fourni
+  // return toutes les données
+  const index = resp.photographers.findIndex(el => el.id == id)
+
+  return(resp.photographers[index])
+
+}
+async function getArticleID(){
+    console.log(new URL(location.href).searchParams.get('id'));
+
+    // Avec l'id, tu vas chercher sur le tableau que tu obtiens quand tu fais ton fetch
+
+    // Utilise la fonction pour récupérer un photographe précis
+    const photographer = await getOnePhotographer(new URL(location.href).searchParams.get('id'))
+    console.log(photographer)
+  return new URL(location.href).searchParams.get('id');
+}
+getArticleID();
+            
