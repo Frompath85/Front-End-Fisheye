ed434e87445b42eab3cdac5ea0d6ab9b418a4eab
baya laadjel <bata_cher@hotmail.fr>
2023-04-15 06:20:23 +0200

    add fetch

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,43 +1,15 @@
     async function getPhotographers() {
 
-        fetch('./data/photographers.json')
-            .then(function(resp){
-                return resp.json()
-            })
-            .then(function(data){
-                console.log(data)
-            })
+        const req = await fetch('./data/photographers.json')
+        const resp = await req.json()
 
-        // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
-        // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois récupéré
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+        return(resp.photographers)
+
     }
 
     async function displayData(photographers) {
         const photographersSection = document.querySelector(".photographer_section");
-
+        
         photographers.forEach((photographer) => {
             const photographerModel = photographerFactory(photographer);
             const userCardDOM = photographerModel.getUserCardDOM();
@@ -47,7 +19,8 @@
 
     async function init() {
         // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
+        const photographers = await getPhotographers();
+        console.log(photographers)
         displayData(photographers);
     };
     
