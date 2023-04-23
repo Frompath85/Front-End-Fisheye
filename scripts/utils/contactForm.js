f946e2e4b175b70f1011692c9c8056d1d6484f58
baya laadjel <bata_cher@hotmail.fr>
2023-04-24 00:29:16 +0200

    test element saisie

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -7,3 +7,37 @@ function closeModal() {
     const modal = document.getElementById("contact_modal");
     modal.style.display = "none";
 }
+
+async function contactPhotographer(){// afficher le nom sur la modale de contact
+    const {name} = await getOnePhotographer();
+    //console.log(name);
+    const namePhotographer = document.getElementById("name_photographer");
+    namePhotographer.innerHTML += `  ${name}` ;
+}
+
+contactPhotographer();
+
+const contactButton = document.querySelector(".contact_button")
+                      .addEventListener("click",envoyer);
+
+function envoyer(){
+    
+    const prenom = document.getElementById("prenom");
+    const nom = document.getElementById("nom");
+    const email = document.getElementById("email");
+
+    const patternName = /^[a-zA-Z]+(?:[- ]?[a-zA-Z]+)*\s*$/;
+    const patternEmail = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+    if(prenom.value.match(patternName)&& nom.value.match(patternName) ){
+        console.log("prenom et nom correct : "+prenom.value + " "+ nom.value );
+    }
+    else 
+    console.log("prenom ou nom incorrect");
+
+    if(email.value.match(patternEmail)){
+        console.log("email correct : "+email.value);
+    }
+    else 
+    console.log("email incorrect");
+ }
