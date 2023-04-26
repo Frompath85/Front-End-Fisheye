1a7524d71d3c8bd18dbbb7952ca483a7492f5152
baya laadjel <bata_cher@hotmail.fr>
2023-04-27 00:51:33 +0200

    test champs ok

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,5 +1,7 @@
 function displayModal() {
     const modal = document.getElementById("contact_modal");
+    const MyForm = document.getElementById("Myform");
+    MyForm.reset();
 	modal.style.display = "block";
 }
 
@@ -14,30 +16,64 @@ async function contactPhotographer(){// afficher le nom sur la modale de contact
     const namePhotographer = document.getElementById("name_photographer");
     namePhotographer.innerHTML += `  ${name}` ;
 }
-
 contactPhotographer();
 
-const contactButton = document.querySelector(".contact_button")
-                      .addEventListener("click",envoyer);
+function envoyer(){//je ferme la modale que si les champs sont correct
 
-function envoyer(){
-    
+    let IsValide = false;
     const prenom = document.getElementById("prenom");
     const nom = document.getElementById("nom");
     const email = document.getElementById("email");
+    const message = document.getElementById("message");
+
+    const MsgErrPrenom = document.querySelector(".ErrPrenom");
+    const MsgErrNom = document.querySelector(".ErrNom");
+    const MsgErrEmail = document.querySelector(".ErrEmail");
+    const MsgErrMessage = document.querySelector(".ErrMessage")
 
     const patternName = /^[a-zA-Z]+(?:[- ]?[a-zA-Z]+)*\s*$/;
     const patternEmail = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+    const patternMessage = /^(?=.{10,})\S+/;
 
-    if(prenom.value.match(patternName)&& nom.value.match(patternName) ){
-        console.log("prenom et nom correct : "+prenom.value + " "+ nom.value );
-    }
-    else 
-    console.log("prenom ou nom incorrect");
+    if(prenom.value.match(patternName)){
+        MsgErrPrenom.style.display = "none";
+        IsValide = true;
+    }else {
+            MsgErrPrenom.style.display = "block";
+            IsValide = false;
+          }
+
+    if(nom.value.match(patternName) ){
+        MsgErrNom.style.display = "none";
+        IsValide = true;
+    } else { 
+             MsgErrNom.style.display = "block";
+             IsValide = false;
+            }
 
     if(email.value.match(patternEmail)){
-        console.log("email correct : "+email.value);
+        MsgErrEmail.style.display = "none";
+        IsValide = true;
+    }else{ 
+           MsgErrEmail.style.display = "block";
+           IsValide = false;
+        }
+
+    if(message.value.match(patternMessage)){
+        MsgErrMessage.style.display = "none";
+        IsValide = true;
+    }else {
+            MsgErrMessage.style.display = "block";
+            IsValide = false;
+        }    
+
+    if (!IsValide){
+        return;
     }
-    else 
-    console.log("email incorrect");
- }
+    console.log("Prénom : "+prenom.value);
+    console.log("Nom : "+ nom.value );
+    console.log("Email : "+email.value);
+    console.log("Message :"+message.value);
+    console.log("données validés, fermeture de la modale");
+    closeModal();
+}          
